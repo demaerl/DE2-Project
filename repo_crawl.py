abceaf157cfbcdf3a94ee25a527b74f773ae87c9
Marion Dübendorfer <marion@duebendorfer.ch>
2023-05-22 16:31:34 +0200

    initial version of repo crawler

--- repo_crawl.py
+++ repo_crawl.py
@@ -1,13 +1,15 @@
 import requests
 import os
+import sys
 
 TOKEN = os.environ['API_TOKEN']
 HEADERS = {'Authorization': f'Bearer {TOKEN}'}
 
 def check_junit(download_url)  -> bool:
     response = requests.get(download_url)
-    if 'junit' in (response.content).decode('utf-8'):
-        return True
+    if response.status_code == 200:
+        if 'junit' in (response.content).decode('utf-8'):
+            return True
     return False
 
 
@@ -31,35 +33,27 @@ def check_pom_xml(repository_url):
  
 
 def main():
+    repo_urls = []
 
-    for i in range(1,10):
+    for i in range(0,10):
+        print(f'Round {i} of 10')
         # Create an API request 
         url = 'https://api.github.com/search/repositories?q=language:java&per_page=100&sort=stars&page='+ str(i)
         response = requests.get(url, headers=HEADERS)
-        print("Status code: ", response.status_code)
-        # In a variable, save the API response.
         response_dict = response.json()
-        # Evaluate the results.
-        print(response_dict.keys())
-
-        print("Total repos:", response_dict['total_count'])
         # find total number of repositories
         repos_dicts = response_dict['items']
-        print("Repos found:", len(repos_dicts))
         # examine the first repository
-        repo_dict = repos_dicts[0]
-
-        print("The following is some information regarding the first repository:")
-        print('Name:', repo_dict['name'])  #print the project's name
-        print('Language:', repo_dict['language'])
-        print('Description:', repo_dict['description']) #print the repository’s description
-        # Call the function to check if pom.xml exists in the repository
-        download_url = check_pom_xml(repo_dict['html_url'])
-        if download_url:
-            print("pom.xml exists in the repository, checking for junit keyword.")
-            if check_junit(download_url):
-                print("pom.xml contains JUnit tests.")
-        print('\n\n')
+        for j in range(0,len(repos_dicts)):
+            repo_dict = repos_dicts[j]
+            # Call the function to check if pom.xml exists in the repository
+            download_url = check_pom_xml(repo_dict['html_url'])
+            if download_url:
+                if check_junit(download_url):
+                    repo_urls.append(repo_dict['clone_url'])
+    
+    print(f'Number of repos that use Maven and JUnit: {len(repo_urls)}')
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()

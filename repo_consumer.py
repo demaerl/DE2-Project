7f6c444b65524b6f78c1190e1e32d87ed52dc60b
Ubuntu <marion@duebendorfer.ch>
2023-05-25 14:57:11 +0000

    add mvn test command

--- repo_consumer.py
+++ repo_consumer.py
@@ -6,21 +6,21 @@ from pulsar import ConsumerType
 BROKER_IP = '192.168.2.138'
 
 def run_tests(url: str):
+    repo_name = url.split('/')[-1][:-4]
     try:
-        subprocess.run(["/usr/bin/git", "clone", url])
+        subprocess.run(['/usr/bin/git', 'clone', url])
     except Exception as e:
         print(f'Error cloning repo {url}: {e}')
+    
+    os.chdir(repo_name)
+    try:
+        print(f'Running tests of repo {repo_name}')
+        subprocess.run(['/usr/bin/mvn', 'test'])
+        os.chdir('..')
+        subprocess.run(['rm', '-rf', repo_name])
+    except Exception as e:
+        print(f'Error running tests of repo {repo_name}: {e}')
 
-    # TODO
-
-# def on_receive(consumer, msg):
-#     try:
-#         repo_url = msg.data().decode('utf-8')
-#         consumer.acknowledge(msg)
-#         print(f'Received repo URL: {repo_url}')
-#         # run_tests(repo_url)
-#     except:
-#         consumer.negative_acknowledge(msg)
 
 def main():
     client = pulsar.Client(f'pulsar://{BROKER_IP}:6650')
@@ -31,14 +31,15 @@ def main():
         consumer_type=ConsumerType.Shared
     )
 
-    msg = consumer.receive()
-    try:
-        repo_url = msg.data().decode('utf-8')
-        consumer.acknowledge(msg)
+    while True:
+        msg = consumer.receive()
+        try:
+            repo_url = msg.data().decode('utf-8')
+            consumer.acknowledge(msg)
+        except:
+            consumer.negative_acknowledge(msg)
         print(f'Received repo URL: {repo_url}')
-        # run_tests(repo_url)
-    except:
-        consumer.negative_acknowledge(msg)
+        run_tests(repo_url)
 
 if __name__ == "__main__":
     main()
